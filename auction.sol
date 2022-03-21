b3e53d7f0a78d366465cc3b6d1336f9656f19731
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-03-21 10:58:28 +0900

    add varable

--- auction.sol
+++ auction.sol
@@ -2,6 +2,19 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Auction {
+    // static
+    address public owner;
+    uint public startBlock;
+    uint public endBlock;
+    string public ipfsHash;
+    uint public bidIncrement;
+    // state
+    bool public canceled;
+    address public highestBidder;
+    mapping(address => uint256) public fundsByBidder;
+    uint public highestBindingBid;
+    bool ownerHaswWithdrawn;
+
     function placeBid() public returns (bool success) {}
     function withdraw() public returns (bool success){}
     function cancelAuction() public returns (bool success){}
